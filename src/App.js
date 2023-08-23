e3046600bab2a5b62d17a3a1f40c3b7f82024296
EddyJCuesta <cuesta79@gmail.com>
2023-08-23 18:16:30 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+//App.js
+import Catalogue from "./components/Catalogue";
+import MovieDetails from "./components/MovieDetails";
+import {Routes, Route } from 'react-router-dom'
+// PseudoCode
+// Set up .ENV file to store our API key
+// Setup _REDIRECTS file 
+
+// Create a Component to hold our initial Axios Call in a useEffect
+// Store our Api Data in STATE
+// Loop over the data and display a movie poster on the page
+
+// Use the LINK to make our POSTERS change the URL on click
+// Use Routes to listen for the url change and display the appropriate component
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>Hackflix!</h1>
+      <Routes>
+        <Route path="/" element={<Catalogue />} />
+        <Route path="/movie/:movieID" element={<MovieDetails />} />
+      </Routes>
+    </>
   );
 }
 
